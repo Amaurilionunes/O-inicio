739b353a0741fe228db7d798ab0eac96c31e9936
Amaurilio Nunes de Azevedo filho <murilo84824187@gmail.com>
2022-09-12 09:10:13 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+var nome = "murilo";
+
+var notaDoPrimeiroBimestre = 9;
+var notaDoSegundoBimestre = 7;
+var notaDoTerceiroBimestre = 5;
+var notaDoQuartoBimestre = 2;
+
+var notaFinal =
+  (notaDoPrimeiroBimestre +
+    notaDoSegundoBimestre +
+    notaDoTerceiroBimestr +
+    notaDoQuartoBimestr /4
+
+var notaFixada = notaFixada.toFixed(1);
+
+console.log("Bem vindo" + nome);
+console.log(notaFinal);
